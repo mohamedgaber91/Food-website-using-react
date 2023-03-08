186e822b06a3f8e8e14d27387732a29931177109
mohamed <2000gaber2000@gmail.com>
2023-03-08 15:32:54 +0200

    modifiy

--- src/components/foods/Foods.js
+++ src/components/foods/Foods.js
@@ -1,14 +1,14 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import food1 from "../../imges/food-1.jpg"
-import food2 from "../../imges/food-2.jpg"
-import food3 from "../../imges/food-3.jpg"
-import food4 from "../../imges/food-4.webp"
-import food5 from "../../imges/food-5.jpg"
-import food6 from "../../imges/food-6.jpg"
-import food7 from "../../imges/food-7.jpg"
-import food8 from "../../imges/food-8.jpg"
-import food9 from "../../imges/food-9.jpg"
+// import food1 from "../../imges/food-1.jpg"
+// import food2 from "../../imges/food-2.jpg"
+// import food3 from "../../imges/food-3.jpg"
+// import food4 from "../../imges/food-4.webp"
+// import food5 from "../../imges/food-5.jpg"
+// import food6 from "../../imges/food-6.jpg"
+// import food7 from "../../imges/food-7.jpg"
+// import food8 from "../../imges/food-8.jpg"
+// import food9 from "../../imges/food-9.jpg"
 import "./Foods.css"
 function Foods (){
     
@@ -21,7 +21,7 @@ function Foods (){
                 </div>
                 <div className="foods-content">
                     <div className="food-1">
-                        <img src={food1} alt=""/>
+                        {/* <img src={food1} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 15 - 20 Minutes | Serves: 1</p>
                         <h3>$10.50 <span className="span">$11.70</span> </h3>
@@ -29,7 +29,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-2">
-                        <img src={food2} alt=""/>
+                        {/* <img src={food2} alt=""/> */}
                         <h5>Vegetarian Burger</h5>
                         <p>Time: 30 - 45 Minutes | Serves: 1</p>
                         <h3>$9.20 <span className="span">$10.50</span> </h3>
@@ -37,7 +37,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-3">
-                        <img src={food3} alt=""/>
+                        {/* <img src={food3} alt=""/> */}
                         <h5>Raspberry Stuffed French Toast</h5>
                         <p>Time: 10 - 15 Minutes | Serves: 1</p>
                         <h3>$12.50 <span className="span">$13.20</span> </h3>
@@ -45,7 +45,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-4">
-                        <img src={food4} alt=""/>
+                        {/* <img src={food4} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 17 - 20 Minutes | Serves: </p>
                         <h3>$10 <span className="span">$15</span> </h3>
@@ -53,7 +53,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-5">
-                        <img src={food5} alt=""/>
+                        {/* <img src={food5} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 4 - 9 Minutes | Serves: 1</p>
                         <h3>$10 <span className="span">$19</span> </h3>
@@ -61,7 +61,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-6">
-                        <img src={food6} alt=""/>
+                        {/* <img src={food6} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 22 - 25 Minutes | Serves: 1</p>
                         <h3>$18 <span className="span">$19</span> </h3>
@@ -69,7 +69,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-7">
-                        <img src={food7} alt=""/>
+                        {/* <img src={food7} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 6 - 98 Minutes | Serves: 1</p>
                         <h3>$51 <span className="span">$55.4</span> </h3>
@@ -77,7 +77,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-8">
-                        <img src={food8} alt=""/>
+                        {/* <img src={food8} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 15 - 20 Minutes | Serves: 1</p>
                         <h3>$20 <span className="span">$25.70</span> </h3>
@@ -85,7 +85,7 @@ function Foods (){
                         <button className="order">order now</button>
                     </div>
                     <div className="food-9">
-                        <img src={food9} alt=""/>
+                        {/* <img src={food9} alt=""/> */}
                         <h5>Rainbow Vegetable Sandwich</h5>
                         <p>Time: 5 - 10 Minutes | Serves: 1</p>
                         <h3>$18 <span className="span">$11.70</span> </h3>
